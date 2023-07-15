abf9539c2bd5fe92d02c14c1d012fea4a3d125a2
Omar Mohamed <107655367+shubra2641@users.noreply.github.com>
2023-07-16 02:09:51 +0300

    www

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://heyawhowa.com/public'),
+    'url' => env('APP_URL', 'https://heyawhowa.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
